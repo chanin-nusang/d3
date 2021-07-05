b31fb341872bc8d12162142b5bf5fbb5cb3f301e
Chanin Nusang <6110110589@psu.ac.th>
2021-07-05 19:39:14 +0700

    2

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,7 +1,14 @@
 import 'package:d3/d3.dart' as d3;
+import 'package:args/args.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${d3.calculate()}!');
+  String state = "Idle";
+  print(arguments.length);
 }
 
-class onTime {}
+class Idle {}
+
+class setHours {}
+
+class setMins {}
