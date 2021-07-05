25a28fa316d81dab2af1bc41c4a7f68946a2109e
Chanin Nusang <6110110589@psu.ac.th>
2021-07-05 19:54:20 +0700

    3

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,10 +1,15 @@
 import 'package:d3/d3.dart' as d3;
 import 'package:args/args.dart';
 
+const lineNumber = 'line-number';
 void main(List<String> arguments) {
   print('Hello world: ${d3.calculate()}!');
   String state = "Idle";
-  print(arguments.length);
+
+  final parser = ArgParser();
+  ArgResults argResults = parser.parse(arguments);
+  final paths = argResults.rest;
+  print(argResults);
 }
 
 class Idle {}
